981edd8168999ef7be9add7d592a6752417718bc
Przemyslaw KADEJ <przemyslaw.kadej-external@rbinternational.com>
2025-08-12 15:00:38 +0200

    update

--- private_dot_config/nvim/lua/plugins/nvim-tree.lua
+++ private_dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -4,16 +4,17 @@ return {
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope.nvim",
   },
   config = function()
     require("nvim-tree").setup {}
     vim.api.nvim_create_autocmd("VimEnter", {
-  pattern = "*",
-  callback = function()
-    if vim.fn.isdirectory(vim.fn.expand("%:p")) == 1 then
-      require'nvim-tree'.open()
-    end
-  end
-})
-  end,
+      pattern = "*",
+      callback = function()
+        if vim.fn.isdirectory(vim.fn.expand("%:p")) == 1 then
+          require'nvim-tree'.open()
+        end
+      end
+    })
+    end,
 }
